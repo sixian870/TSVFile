45d2d97cb32432f838270e88aac905c58d2a5e90
sixian870 <alice0701lee@gmail.com>
2026-05-24 17:41:44 +0800

    加入專案檔案。

---
+++ TSVFile/frmTSVFile.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace TSVFile
+{
+    partial class frmTSVFile
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mnsWord = new System.Windows.Forms.MenuStrip();
+            this.tsmiFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.ssrWord = new System.Windows.Forms.StatusStrip();
+            this.tsslMessage = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lvwWord = new System.Windows.Forms.ListView();
+            this.chWord = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chPhonogram = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chSoundPath = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chExplain = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.pnlSearch = new System.Windows.Forms.Panel();
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.mnsWord.SuspendLayout();
+            this.ssrWord.SuspendLayout();
+            this.pnlSearch.SuspendLayout();
+            this.pnlMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mnsWord
+            // 
+            this.mnsWord.AutoSize = false;
+            this.mnsWord.BackColor = System.Drawing.Color.AliceBlue;
+            this.mnsWord.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mnsWord.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.mnsWord.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiFile,
+            this.tsmiHelp});
+            this.mnsWord.Location = new System.Drawing.Point(0, 0);
+            this.mnsWord.Name = "mnsWord";
+            this.mnsWord.Padding = new System.Windows.Forms.Padding(5, 1, 0, 1);
+            this.mnsWord.Size = new System.Drawing.Size(128, 31);
+            this.mnsWord.TabIndex = 0;
+            this.mnsWord.Text = "File";
+            // 
+            // tsmiFile
+            // 
+            this.tsmiFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiOpen,
+            this.tsmiExit});
+            this.tsmiFile.Name = "tsmiFile";
+            this.tsmiFile.Size = new System.Drawing.Size(57, 29);
+            this.tsmiFile.Text = "檔案(&F)";
+            // 
+            // tsmiOpen
+            // 
+            this.tsmiOpen.Name = "tsmiOpen";
+            this.tsmiOpen.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.tsmiOpen.ShowShortcutKeys = false;
+            this.tsmiOpen.Size = new System.Drawing.Size(180, 22);
+            this.tsmiOpen.Text = "開啟(&O)";
+            this.tsmiOpen.Click += new System.EventHandler(this.tsmiOpen_Click);
+            // 
+            // tsmiExit
+            // 
+            this.tsmiExit.Name = "tsmiExit";
+            this.tsmiExit.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+            this.tsmiExit.ShowShortcutKeys = false;
+            this.tsmiExit.Size = new System.Drawing.Size(180, 22);
+            this.tsmiExit.Text = "離開(&X)";
+            this.tsmiExit.Click += new System.EventHandler(this.tsmiExit_Click);
+            // 
+            // tsmiHelp
+            // 
+            this.tsmiHelp.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiAbout});
+            this.tsmiHelp.Name = "tsmiHelp";
+            this.tsmiHelp.Size = new System.Drawing.Size(60, 29);
+            this.tsmiHelp.Text = "幫助(&H)";
+            // 
+            // tsmiAbout
+            // 
+            this.tsmiAbout.Name = "tsmiAbout";
+            this.tsmiAbout.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.A)));
+            this.tsmiAbout.ShowShortcutKeys = false;
+            this.tsmiAbout.Size = new System.Drawing.Size(180, 22);
+            this.tsmiAbout.Text = "關於(&A)";
+            this.tsmiAbout.Click += new System.EventHandler(this.tsmiAbout_Click);
+            // 
+            // ssrWord
+            // 
+            this.ssrWord.BackColor = System.Drawing.SystemColors.Window;
+            this.ssrWord.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsslMessage});
+            this.ssrWord.Location = new System.Drawing.Point(0, 343);
+            this.ssrWord.Name = "ssrWord";
+            this.ssrWord.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.ssrWord.Size = new System.Drawing.Size(587, 22);
+            this.ssrWord.TabIndex = 1;
+            // 
+            // tsslMessage
+            // 
+            this.tsslMessage.Name = "tsslMessage";
+            this.tsslMessage.Size = new System.Drawing.Size(58, 17);
+            this.tsslMessage.Text = "Message";
+            // 
+            // lvwWord
+            // 
+            this.lvwWord.BackColor = System.Drawing.Color.AliceBlue;
+            this.lvwWord.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chWord,
+            this.chPhonogram,
+            this.chSoundPath,
+            this.chExplain});
+            this.lvwWord.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvwWord.FullRowSelect = true;
+            this.lvwWord.HideSelection = false;
+            this.lvwWord.Location = new System.Drawing.Point(0, 31);
+            this.lvwWord.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lvwWord.Name = "lvwWord";
+            this.lvwWord.Size = new System.Drawing.Size(587, 312);
+            this.lvwWord.TabIndex = 2;
+            this.lvwWord.UseCompatibleStateImageBehavior = false;
+            this.lvwWord.View = System.Windows.Forms.View.Details;
+            this.lvwWord.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.lvwWord_ColumnClick);
+            // 
+            // chWord
+            // 
+            this.chWord.Text = "單字";
+            // 
+            // chPhonogram
+            // 
+            this.chPhonogram.Text = "音標";
+            // 
+            // chSoundPath
+            // 
+            this.chSoundPath.Text = "音檔路徑";
+            this.chSoundPath.Width = 75;
+            // 
+            // chExplain
+            // 
+            this.chExplain.Text = "解釋";
+            // 
+            // pnlSearch
+            // 
+            this.pnlSearch.BackColor = System.Drawing.Color.AliceBlue;
+            this.pnlSearch.Controls.Add(this.txtSearch);
+            this.pnlSearch.Controls.Add(this.lblSearch);
+            this.pnlSearch.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnlSearch.Location = new System.Drawing.Point(128, 0);
+            this.pnlSearch.Name = "pnlSearch";
+            this.pnlSearch.Size = new System.Drawing.Size(459, 31);
+            this.pnlSearch.TabIndex = 3;
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Font = new System.Drawing.Font("微軟正黑體", 9F);
+            this.lblSearch.Location = new System.Drawing.Point(3, 8);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(64, 16);
+            this.lblSearch.TabIndex = 0;
+            this.lblSearch.Text = "查詢單字 : ";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.BackColor = System.Drawing.SystemColors.Window;
+            this.txtSearch.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtSearch.Location = new System.Drawing.Point(70, 7);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(372, 18);
+            this.txtSearch.TabIndex = 1;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.Controls.Add(this.mnsWord);
+            this.pnlMain.Controls.Add(this.pnlSearch);
+            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlMain.Location = new System.Drawing.Point(0, 0);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(587, 31);
+            this.pnlMain.TabIndex = 4;
+            // 
+            // frmTSVFile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 365);
+            this.Controls.Add(this.lvwWord);
+            this.Controls.Add(this.ssrWord);
+            this.Controls.Add(this.pnlMain);
+            this.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.MainMenuStrip = this.mnsWord;
+            this.Name = "frmTSVFile";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TVS檔案格式讀取程式";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmTSVFile_FormClosing);
+            this.Load += new System.EventHandler(this.frmTSVFile_Load);
+            this.mnsWord.ResumeLayout(false);
+            this.mnsWord.PerformLayout();
+            this.ssrWord.ResumeLayout(false);
+            this.ssrWord.PerformLayout();
+            this.pnlSearch.ResumeLayout(false);
+            this.pnlSearch.PerformLayout();
+            this.pnlMain.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnsWord;
+        private System.Windows.Forms.ToolStripMenuItem tsmiFile;
+        private System.Windows.Forms.ToolStripMenuItem tsmiHelp;
+        private System.Windows.Forms.StatusStrip ssrWord;
+        private System.Windows.Forms.ToolStripStatusLabel tsslMessage;
+        private System.Windows.Forms.ListView lvwWord;
+        private System.Windows.Forms.ColumnHeader chWord;
+        private System.Windows.Forms.ColumnHeader chPhonogram;
+        private System.Windows.Forms.ColumnHeader chSoundPath;
+        private System.Windows.Forms.ColumnHeader chExplain;
+        private System.Windows.Forms.ToolStripMenuItem tsmiOpen;
+        private System.Windows.Forms.ToolStripMenuItem tsmiExit;
+        private System.Windows.Forms.ToolStripMenuItem tsmiAbout;
+        private System.Windows.Forms.Panel pnlSearch;
+        private System.Windows.Forms.Label lblSearch;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Panel pnlMain;
+    }
+}
+
---
+++ TSVFile/frmTSVFile.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TSVFile
+{
+    public partial class frmTSVFile : Form
+    {
+        ///<summary>
+        ///關於視窗
+        ///</summary>
+        frmAbout about =new frmAbout();
+
+        ///<summary>
+        ///單字清單
+        ///</summary>
+        WordCollection _WordList= new WordCollection();
+
+        public frmTSVFile()
+        {
+            InitializeComponent();
+        }
+
+        ///<summary>
+        ///更新ListView的內容
+        ///</summary>
+        private void UpdateListView(string keyword = "")
+        {
+            lvwWord.BeginUpdate(); //暫停重繪
+            lvwWord.Items.Clear();// 清除ListView的所有項目
+
+            // 將WordCollection物件中的資料載入到ListView中
+            foreach (WordItem item in _WordList)
+            {
+                if (item == null) continue;
+
+                // 如果有輸入關鍵字，且單字本身「不包含」這個關鍵字（不區分大小寫），就跳過不顯示
+                if (!string.IsNullOrEmpty(keyword) &&
+                    item.Word.IndexOf(keyword, StringComparison.OrdinalIgnoreCase) < 0)
+                {
+                    continue;
+                }
+
+                // 建立ListViewItem物件
+                ListViewItem lvi = new ListViewItem(item.Word);
+                lvi.SubItems.Add(item.Phonogram ?? "");
+                lvi.SubItems.Add(item.SoundPath ?? "");
+                lvi.SubItems.Add(item.Explain ?? "");
+                // 將ListViewItem物件加入到ListView中
+                lvwWord.Items.Add(lvi);
+            }
+            // 自動調整所有欄位寬度以符合內容長度
+            if (lvwWord.Items.Count > 0)
+            {
+                lvwWord.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+            }
+            else
+            {
+                lvwWord.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+            }
+
+            lvwWord.EndUpdate(); //重繪;
+            // 即時更新狀態列顯示目前搜尋到的數量
+            tsslMessage.Text = string.IsNullOrEmpty(keyword) ?
+                $"成功載入 {_WordList.Count} 個單字" :
+                $"找到 {lvwWord.Items.Count} 筆符合「{keyword}」的結果";
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            // 把文字方塊目前輸入的內容傳給 UpdateListView 進行過濾
+            UpdateListView(txtSearch.Text.Trim());
+        }
+
+        private void frmTSVFile_Load(object sender, EventArgs e)
+        {
+            tsslMessage.Text = "請開啟檔案";
+        }
+
+        private void tsmiAbout_Click(object sender, EventArgs e)
+        {
+            about.ShowDialog(this);
+        }
+
+        private void tsmiOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Textfiles (*.txt)|*.txt|TSV files (*.tsv)|*.tsv|Allfiles (*.*)|*.*";
+            ofd.Title = "開啟檔案";
+            // 設定初始目錄為程式所在目錄
+            ofd.InitialDirectory = Application.StartupPath;
+            DialogResult dr = ofd.ShowDialog(this);
+            if (dr == DialogResult.OK)
+            {
+                // 讀取檔案並且將每一行的資料放入字串陣列
+                string[] lines = File.ReadAllLines(ofd.FileName, Encoding.UTF8);
+                // 將字串陣列的資料載入到WordCollection物件中
+                _WordList.LoadFromStringArray(lines);
+                // 將WordCollection物件中的資料載入到ListView中
+                UpdateListView();
+                this.tsslMessage.Text = $"{_WordList.Count} 單字已成功載入";
+            }
+        }
+
+        private void tsmiExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmTSVFile_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("確定要離開嗎?", "離開", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dr == DialogResult.No)
+            {
+                e.Cancel = true; // 取消關閉
+            }
+        }
+
+        // 用來記錄目前是正向排序還是反向排序
+        private bool isAscending = true;
+
+        private void lvwWord_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            isAscending = !isAscending; // 切換正反向狀態
+            lvwWord.ListViewItemSorter = new ListViewItemComparer(e.Column, isAscending); // 設定 ListView 的排序器
+            lvwWord.Sort(); // 執行排序
+        }
+
+        public class ListViewItemComparer : System.Collections.IComparer
+        {
+            private int col;
+            private bool asc;
+
+            public ListViewItemComparer(int column, bool ascending)
+            {
+                col = column;
+                asc = ascending;
+            }
+
+            public int Compare(object x, object y)
+            {
+                int returnVal = string.Compare(((ListViewItem)x).SubItems[col].Text,
+                                               ((ListViewItem)y).SubItems[col].Text);
+
+                // 如果是反向排序，就把結果乘以 -1
+                return asc ? returnVal : -returnVal;
+            }
+        }
+    }
+}
